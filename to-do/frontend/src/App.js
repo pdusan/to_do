438f808a503adfcae7d3992cb6b2528a6afcc98f
Dusan <dusanp96@gmail.com>
2022-08-21 00:26:53 +0200

    restructured frontend

--- to-do/frontend/src/App.js
+++ to-do/frontend/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./static/App.css";
 import { Component } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Home";
